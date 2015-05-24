32df7712a57708f1648a1f47731e4f324965f9f8
Marwan Sarieddine <sarieddine.marwan@gmail.com>
2015-05-24 19:23:03 -0400

    Static Post Page Done

---
+++ client/posts/postList.js
@@ -0,0 +1,8 @@
+var	postsData=[{title: "google",
+										url: "http://www.google.com"},
+									{	title: "youtube",
+										url: "http://www.youtube.com"}];
+
+Template.postList.helpers({
+	 posts : postsData
+});
